b5070cf4e1f16e577c585918cc303fe843a2151b
Anantha018 <63914049+Anantha018@users.noreply.github.com>
2024-06-24 01:52:16 -0400

    Update integrate_app.py

--- integrate_app.py
+++ integrate_app.py
@@ -7,7 +7,7 @@ import json, requests, re
 
 app = Flask(__name__)
 
-DEVELOPER_KEY = 'AIzaSyD7KgygEbYsJgDiPKLca2TmFffoJuqdScY'  
+DEVELOPER_KEY = os.getenv('YOUTUBE_API_KEY')
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
