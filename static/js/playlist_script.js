d1e42dfa86ac544565bc0e7749650b6f2f2298d5
Anantha018 <63914049+Anantha018@users.noreply.github.com>
2024-07-17 16:26:21 -0400

    Update playlist_script.js

--- static/js/playlist_script.js
+++ static/js/playlist_script.js
@@ -34,11 +34,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     audioPlayer.classList.add('audioPlayer', 'active');
                     audioPlayer.innerHTML = `
                         <h2>${title}</h2>
-                        <audio id="audioPlayer" controls preload="metadata">
-                            <source src="${data.audio_url_ogg}" type="audio/ogg">
-                            <source src="${data.audio_url_mp3}" type="audio/mpeg">
-                            Your browser does not support the audio element.
-                        </audio>
+                          <audio controls autoplay>
+                                <source src="${data.audio_url}" type="audio/mpeg">
+                                Your browser does not support the audio element.
+                            </audio>
                         
                         <div class="audio-controls">
                             <button class="prev-btn"><i class="fas fa-step-backward"></i></button>
