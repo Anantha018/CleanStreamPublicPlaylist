956a455f6b9a20c27066511d9626da176db3846c
Anantha018 <63914049+Anantha018@users.noreply.github.com>
2024-08-20 02:34:53 -0400

    Update integrate_app.py
    Changed Port and IP address to deploy in railway app

--- integrate_app.py
+++ integrate_app.py
@@ -192,5 +192,8 @@ def audio(video_id):
         # Handle exceptions and return a 500 error response with the exception message
         return jsonify({'error': str(e)}), 500
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
